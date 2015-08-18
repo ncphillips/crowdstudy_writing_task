65e2d8adaa2905c2c1fca091e1a0e2d0a275ccfb
Scott Bateman <sbateman@upei.ca>
2015-08-18 15:13:41 -0300

    Updated questions to have scale for effort and performance

--- public/scripts/Views/Questions.jsx
+++ public/scripts/Views/Questions.jsx
@@ -4,9 +4,8 @@ var _questions = {
     "Did you write your last " + CONFIG.block_size + " stories quickly?",
     "Were your last " + CONFIG.block_size + " stories a good length?",
     "Were your last " + CONFIG.block_size + " stories creative?",
-    "Do you feel like you performed well when writing your last "+ CONFIG.block_size + " stories?", 
-    "Did you try hard, when writing your last "+ CONFIG.block_size +" stories?"
-
+    "On a scale from 1 to 10, how well when writing your last "+ CONFIG.block_size + " stories? [1 = \"I did not perform well at all\", 10 = \"I performed as well as I could\"]", 
+    "On a scale from 1 to 10, how hard did you try when writing your last "+ CONFIG.block_size + " stories? [1 = \"I did not try at all\", 10 = \"I tried as hard as I could\"]" 
   ],
   comparison: [
     "Did you write your last " + CONFIG.block_size + " stories faster than the Average worker?",
@@ -15,8 +14,8 @@ var _questions = {
     "Were your last " + CONFIG.block_size + " stories longer than the Expert worker's?",
     "Although you haven't evaluated your stories yet, do you think that your last " + CONFIG.block_size + " stories were more creative than the Average worker's?",
     "Although you haven't evaluated your stories yet, do you think that your last " + CONFIG.block_size + " stories were more creative than the Expert worker's?",
-    "Do you feel like you performed well when writing your last "+ CONFIG.block_size + " stories?",
-    "Did you try hard, when writing your last "+ CONFIG.block_size +" stories?"
+    "On a scale from 1 to 10, how well when writing your last "+ CONFIG.block_size + " stories? [1 = \"I did not perform well at all\", 10 = \"I performed as well as I could\"]", 
+    "On a scale from 1 to 10, how hard did you try when writing your last "+ CONFIG.block_size + " stories? [1 = \"I did not try at all\", 10 = \"I tried as hard as I could\"]" 
   ]
 };
 
