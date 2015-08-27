b83276d5b140cdda750f8bdca2b23a19d1d715ce
ncphillips <ncphillips@upei.ca>
2015-08-27 13:57:50 -0300

    StoryActions fix

--- public/scripts/Actions/StoryActions.jsx
+++ public/scripts/Actions/StoryActions.jsx
@@ -1,8 +1,10 @@
 'use strict';
 
 var StoryActions = {
+  _updatingStory: false,
+  _nextAction: {actionType: '',},
   update: function (iid, text, edit) {
-    var action = {
+    StoryActions._nextAction = {
       actionType: STORY_ACTIONS.UPDATE,
       iid: iid,
       text: text,
@@ -22,12 +24,26 @@ var StoryActions = {
     edit.time_end = Number(edit.time_end);
     experiment.data[iid].edits.push(edit);
 
-
+    StoryActions._updatingStory = true;
     ExperimentActions.update(worker._id, 'writing_task', experiment);
-    CrowdDispatcher.dispatch(action);
+  },
+  _dispatchUpdate: function(){
+    /**
+     * Gets called anytime an Experiment is updated, and dispatches if
+     * StoryActions is currently updating, which means that the Experiment
+     * update is a result of this thing or whatever.
+     */
+    var experiment = ExperimentStore.get();
+    var image = ImageStore.getCurrent();
+    if (StoryActions._updatingStory &&
+      StoryActions._nextAction.iid >= 0 &&
+      experiment.data.length > StoryActions._nextAction.iid) {
+      StoryActions._updatingStory = false;
+      CrowdDispatcher.dispatch(StoryActions._nextAction);
+    }
   }
 };
-
+ExperimentStore.addChangeListener(StoryActions._dispatchUpdate);
 
 var StoryStatsActions = (function() {
   function setStats(data) {
@@ -44,16 +60,21 @@ var StoryStatsActions = (function() {
       if (typeof block_size === 'undefined') {
         block_size = CONFIG.block_size;
       }
+      var experiment = ExperimentStore.get();
+      if (!StoryActions._updatingStory && experiment.data.length >= (1 + block_num) * block_size) {
+        $.ajax({
+          type: 'GET',
+          url: '/writing_task/' + wid + '/stats?block_num=' + block_num + '&block_size=' + block_size,
+          dataType: 'json',
+          success: setStats,
+          error: function (a, b, c) {
+            console.log(a, b, c);
+          }
+        });
+      } else {
 
-      $.ajax({
-        type: 'GET',
-        url: '/writing_task/' + wid + '/stats?block_num=' + block_num + '&block_size=' + block_size,
-        dataType: 'json',
-        success: setStats,
-        error: function (a, b, c) {
-          console.log(a, b, c);
-        }
-      });
+        setTimeout(StoryStatsActions.load_block_stats.bind(null, wid, block_num, block_size), 100);
+      }
     }
   };
 })();
